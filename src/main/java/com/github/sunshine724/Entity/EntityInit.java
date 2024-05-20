84c3d17c9a58165ba7e49922598bce410712e6a6
auksu <raiki136@gmail.com>
2024-05-20 13:47:25 +0900

    担当部分は完成です。 JarijariMod.javaの部分に、アイテムレジストリをイベントバスに追加・動画と見比べてなかった部分を左で述べたものの上部分にも追加しました

--- src/main/java/com/github/sunshine724/Entity/EntityInit.java
+++ src/main/java/com/github/sunshine724/Entity/EntityInit.java
@@ -1,24 +1,24 @@
-//package com.github.sunshine724.Entity;
-//
-//import com.github.sunshine724.JarijariMod.JarijariMod;
-//import com.github.sunshine724.Entity.JarijariEntity;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.entity.EntityType;
-//import net.minecraft.world.entity.MobCategory;
-//import net.minecraft.world.item.Item;
-//import net.minecraftforge.registries.DeferredRegister;
-//import net.minecraftforge.registries.ForgeRegistries;
-//import net.minecraftforge.registries.RegistryObject;
-//
-///*エンティティの初期化クラス*/
-//public class EntityInit {
-//    /*レジスタにエンティティを登録する*/
-//    public static final DeferredRegister<EntityType<?>> ENTITYS = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES,JarijariMod.MODID);
-//
-//    //モブカテゴリとサイズとモブの名前を登録する
-//    public static final RegistryObject<EntityType<JarijariEntity>> JARIJARI_ENTITY = ENTITYS.register("Jarijari_entity",
-//            () -> EntityType.Builder.<JarijariEntity>of(JarijariEntity::new, MobCategory.CREATURE)
-//                    .sized(1.0f,1.0f)
-//                    .build(new ResourceLocation(JarijariMod.MODID,"Jarijari_entity").toString()));
-//
-//}
+package com.github.sunshine724.Entity;
+
+import com.github.sunshine724.JarijariMod.JarijariMod;
+import com.github.sunshine724.Entity.JarijariEntity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+/*エンティティの初期化クラス*/
+public class EntityInit {
+    /*レジスタにエンティティを登録する*/
+    public static final DeferredRegister<EntityType<?>> ENTITYS = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES,JarijariMod.MODID);
+
+    //モブカテゴリとサイズとモブの名前を登録する
+    public static final RegistryObject<EntityType<JarijariEntity>> JARIJARI_ENTITY = ENTITYS.register("Jarijari_entity",
+            () -> EntityType.Builder.<JarijariEntity>of(JarijariEntity::new, MobCategory.CREATURE)
+                    .sized(1.0f,1.0f)
+                    .build(new ResourceLocation(JarijariMod.MODID,"Jarijari_entity").toString()));
+
+}
